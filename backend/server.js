0321749bd34848412ec5686717d8fbaf0e89c1f5
Priyanshu Rajpurohit <officialpriyanshu14@gmail.com>
2025-03-04 21:43:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/checklists", checklistRoutes); // Use the checklist routes
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
